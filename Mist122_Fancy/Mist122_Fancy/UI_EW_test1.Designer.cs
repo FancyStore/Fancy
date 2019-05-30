f71c82cfd6392c23378acd2ce2e703a5404b2d06
ewingtsui <ewing.tsui@gmail.com>
2019-05-30 14:16:47 +0800

    Ewing: 新增Form UI_EW_test2
    首發測試   Git Foke

--- Mist122_Fancy/Mist122_Fancy/UI_EW_test1.Designer.cs
+++ Mist122_Fancy/Mist122_Fancy/UI_EW_test1.Designer.cs
@@ -1,16 +1,16 @@
 ﻿namespace Mist122_Fancy
 {
-    partial class Form1
+    partial class UI_EW_test1
     {
         /// <summary>
-        /// 設計工具所需的變數。
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// 清除任何使用中的資源。
+        /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,11 +20,11 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form 設計工具產生的程式碼
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
-        /// 這個方法的內容。
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
@@ -34,29 +34,29 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(91, 50);
+            this.button1.Location = new System.Drawing.Point(62, 46);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(116, 51);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(257, 59);
+            this.textBox1.Location = new System.Drawing.Point(210, 33);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 1;
             // 
-            // Form1
+            // UI_EW_test1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "UI_EW_test1";
+            this.Text = "Form2";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -68,4 +68,3 @@
         private System.Windows.Forms.TextBox textBox1;
     }
 }
-
